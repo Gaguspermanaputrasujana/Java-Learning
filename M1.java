82ebc5cc2408dfba6d26fd9a991913da60dd4e43
G. APPS <78084264+Gaguspermanaputrasujana@users.noreply.github.com>
2021-02-27 18:33:02 +0800

    Add files via upload
    Training on using classes and creating object. toned down for simplicity

---
+++ M1.java
@@ -0,0 +1,19 @@
+package random;
+
+class  Dog {
+	int ma (int x, int y) {
+int z;
+	z = x + y;
+	System.out.print(z);
+	z = z +2;
+	return z;
+	}
+	int mo (int x, int y) {
+		int d;
+		d = (x * x) + (2 * y);
+		System.out.print(d+ " ");
+		d = d + 2;
+		return d;
+	}
+}
+
---
+++ Train4A.java
@@ -0,0 +1,16 @@
+package random;
+
+
+class Ama {
+	String name;
+	
+	public static void main (String[] args) {
+		int nume1; int nume2;
+		Dog alin= new Dog();
+		nume1= 12; nume2 =20;
+		
+	System.out.print(alin.mo(nume1, nume2));
+		}
+		
+	
+}
