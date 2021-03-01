21ab9dab9979c6ffc120d3414f8efd17a0b9baeb
G. APPS <78084264+Gaguspermanaputrasujana@users.noreply.github.com>
2021-03-01 16:35:01 +0800

    Add files via upload
    2 similar number counting. refreshing on number count and accesing value and methods

---
+++ access1.java
@@ -0,0 +1,39 @@
+package p1;
+
+public class access1 {
+	 int counter = 0;
+	 
+	public static void main(String[] args) {
+		int count = 0; 
+	Mix4 [] m4a = new Mix4[20];
+	int x = 0;
+	while (x < 9 ) {
+		m4a[x] = new Mix4();
+		m4a[x].counter = m4a[x].counter + 1;
+		count = count + 1;
+		count = count + m4a[x].maybeNew(x);
+		x = x + 1;
+		System.out.println(x +" "+ count + " " + maybeNew(x)+ " "+ Ming("hona"));
+	}
+	System.out.println(count + " " + m4a[1].counter);
+	}
+
+	
+
+	
+	public static int maybeNew (int index)
+	{
+				{
+		if ( index < 5 ) {
+			Mix4 m4 = new Mix4();
+			m4.counter = m4.counter + 1;
+			return 1;
+		} return 0;
+	}}
+
+public static String Ming ( String dr)
+{
+	
+	String bind = dr + " moka";
+	return bind;
+}}
