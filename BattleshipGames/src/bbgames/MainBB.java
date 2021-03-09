de6cdc74cf51f42fff659b2a4780100d6cbb79b2
G. APPS <78084264+Gaguspermanaputrasujana@users.noreply.github.com>
2021-03-09 18:55:50 +0800

    Randomized BB games from Headfirst book
    1st version, similar to dot.com games. im learning usage of of array and arraylist.

---
+++ BattleshipGames/src/bbgames/MainBB.java
@@ -0,0 +1,21 @@
+package bbgames;
+
+public class MainBB 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		int p = MainBB.guess(10);
+		System.out.print(p + " ");
+		System.out.print( RanBB.RN());
+
+	
+	}
+	public static int guess(int imo)
+	{ int belavi = imo + 2;
+		int belavid = belavi;
+		return belavid;
+	}
+}
+
---
+++ BattleshipGames/src/bbgames/RanBB.java
@@ -0,0 +1,13 @@
+package bbgames;
+
+public class RanBB {
+	
+	
+	static int RN ()
+	{ 
+		int ms = (int) (Math.random()*10);
+		
+		return ms;
+	}
+}
+
